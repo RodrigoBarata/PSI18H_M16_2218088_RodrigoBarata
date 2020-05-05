081d370ba857373148e909efd58395d12fb7d525
RodrigoBarata <58223438+RodrigoBarata@users.noreply.github.com>
2020-05-05 18:54:45 +0100

    Add files via upload

---
+++ src/PSI18H_M16_Projeto_2218088_RodrigoBarata/PSI18H_M16_Projeto_2218088_RodrigoBarata/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace PSI18H_M16_Projeto_2218088_RodrigoBarata
+{
+    partial class DBViewer
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DBViewer));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.clientes = new System.Windows.Forms.Button();
+            this.cursos = new System.Windows.Forms.Button();
+            this.formadores = new System.Windows.Forms.Button();
+            this.formaçoes = new System.Windows.Forms.Button();
+            this.shutdown = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.check = new System.Windows.Forms.Button();
+            this.Registar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.formaçoes);
+            this.panel1.Controls.Add(this.formadores);
+            this.panel1.Controls.Add(this.cursos);
+            this.panel1.Controls.Add(this.clientes);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(193, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Maroon;
+            this.panel2.Controls.Add(this.shutdown);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(193, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(607, 105);
+            this.panel2.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(187, 102);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // clientes
+            // 
+            this.clientes.BackColor = System.Drawing.Color.Transparent;
+            this.clientes.FlatAppearance.BorderSize = 0;
+            this.clientes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.clientes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.clientes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clientes.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clientes.ForeColor = System.Drawing.Color.White;
+            this.clientes.Image = ((System.Drawing.Image)(resources.GetObject("clientes.Image")));
+            this.clientes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.clientes.Location = new System.Drawing.Point(12, 148);
+            this.clientes.Name = "clientes";
+            this.clientes.Size = new System.Drawing.Size(193, 47);
+            this.clientes.TabIndex = 1;
+            this.clientes.Text = "Clientes";
+            this.clientes.UseVisualStyleBackColor = false;
+            // 
+            // cursos
+            // 
+            this.cursos.BackColor = System.Drawing.Color.Transparent;
+            this.cursos.FlatAppearance.BorderSize = 0;
+            this.cursos.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.cursos.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.cursos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cursos.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cursos.ForeColor = System.Drawing.Color.White;
+            this.cursos.Image = ((System.Drawing.Image)(resources.GetObject("cursos.Image")));
+            this.cursos.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.cursos.Location = new System.Drawing.Point(12, 202);
+            this.cursos.Name = "cursos";
+            this.cursos.Size = new System.Drawing.Size(193, 47);
+            this.cursos.TabIndex = 4;
+            this.cursos.Text = "Cursos";
+            this.cursos.UseVisualStyleBackColor = false;
+            // 
+            // formadores
+            // 
+            this.formadores.BackColor = System.Drawing.Color.Transparent;
+            this.formadores.FlatAppearance.BorderSize = 0;
+            this.formadores.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.formadores.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.formadores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.formadores.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.formadores.ForeColor = System.Drawing.Color.White;
+            this.formadores.Image = ((System.Drawing.Image)(resources.GetObject("formadores.Image")));
+            this.formadores.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.formadores.Location = new System.Drawing.Point(12, 255);
+            this.formadores.Name = "formadores";
+            this.formadores.Size = new System.Drawing.Size(193, 47);
+            this.formadores.TabIndex = 5;
+            this.formadores.Text = "Formadores";
+            this.formadores.UseVisualStyleBackColor = false;
+            // 
+            // formaçoes
+            // 
+            this.formaçoes.BackColor = System.Drawing.Color.Transparent;
+            this.formaçoes.FlatAppearance.BorderSize = 0;
+            this.formaçoes.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.formaçoes.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.formaçoes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.formaçoes.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.formaçoes.ForeColor = System.Drawing.Color.White;
+            this.formaçoes.Image = ((System.Drawing.Image)(resources.GetObject("formaçoes.Image")));
+            this.formaçoes.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.formaçoes.Location = new System.Drawing.Point(9, 308);
+            this.formaçoes.Name = "formaçoes";
+            this.formaçoes.Size = new System.Drawing.Size(193, 47);
+            this.formaçoes.TabIndex = 6;
+            this.formaçoes.Text = "Formações";
+            this.formaçoes.UseVisualStyleBackColor = false;
+            // 
+            // shutdown
+            // 
+            this.shutdown.BackColor = System.Drawing.Color.Transparent;
+            this.shutdown.FlatAppearance.BorderSize = 0;
+            this.shutdown.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.shutdown.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.shutdown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.shutdown.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shutdown.ForeColor = System.Drawing.Color.White;
+            this.shutdown.Image = ((System.Drawing.Image)(resources.GetObject("shutdown.Image")));
+            this.shutdown.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.shutdown.Location = new System.Drawing.Point(550, 12);
+            this.shutdown.Name = "shutdown";
+            this.shutdown.Size = new System.Drawing.Size(45, 47);
+            this.shutdown.TabIndex = 7;
+            this.shutdown.UseVisualStyleBackColor = false;
+            this.shutdown.Click += new System.EventHandler(this.shutdown_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(0, 148);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(14, 47);
+            this.panel3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(383, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(381, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(387, 188);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox2.Location = new System.Drawing.Point(387, 241);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // check
+            // 
+            this.check.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.check.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.check.Location = new System.Drawing.Point(407, 279);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(126, 23);
+            this.check.TabIndex = 5;
+            this.check.Text = "Confirmar";
+            this.check.UseVisualStyleBackColor = false;
+            // 
+            // Registar
+            // 
+            this.Registar.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Registar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.Registar.Location = new System.Drawing.Point(423, 308);
+            this.Registar.Name = "Registar";
+            this.Registar.Size = new System.Drawing.Size(98, 23);
+            this.Registar.TabIndex = 6;
+            this.Registar.Text = "Registar";
+            this.Registar.UseVisualStyleBackColor = false;
+            // 
+            // DBViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Registar);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "DBViewer";
+            this.Text = "DBViewer";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button formaçoes;
+        private System.Windows.Forms.Button formadores;
+        private System.Windows.Forms.Button cursos;
+        private System.Windows.Forms.Button clientes;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button shutdown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.Button Registar;
+    }
+}
+
---
+++ src/PSI18H_M16_Projeto_2218088_RodrigoBarata/PSI18H_M16_Projeto_2218088_RodrigoBarata/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PSI18H_M16_Projeto_2218088_RodrigoBarata
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new DBViewer());
+        }
+    }
+}
