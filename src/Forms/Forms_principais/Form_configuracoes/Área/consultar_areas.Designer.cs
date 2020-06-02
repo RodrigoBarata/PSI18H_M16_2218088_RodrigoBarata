d8e6b73ace4c9675f2b71aa53a009b173c778b78
Rodrigo Guerreiro Baptista Silva Barata <2218088@alunos.escoladigital.com>
2020-06-02 18:11:59 +0100

    versao 0.6

--- src/Forms/Forms_principais/Form_configuracoes/Área/consultar_areas.Designer.cs
+++ src/Forms/Forms_principais/Form_configuracoes/Área/consultar_areas.Designer.cs
@@ -29,72 +29,100 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btneditar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtconsultararea = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtarea = new System.Windows.Forms.TextBox();
+            this.btnremover = new System.Windows.Forms.Button();
             this.btnnovo = new System.Windows.Forms.Button();
             this.dtareas = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btneditar = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtareas)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Transparent;
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.dtareas);
-            this.panel1.Controls.Add(this.iconPictureBox1);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.btneditar);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtconsultararea);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.txtarea);
+            this.panel1.Controls.Add(this.btnremover);
             this.panel1.Controls.Add(this.btnnovo);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Controls.Add(this.dtareas);
+            this.panel1.Controls.Add(this.btneditar);
+            this.panel1.Location = new System.Drawing.Point(130, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(737, 424);
+            this.panel1.Size = new System.Drawing.Size(541, 424);
             this.panel1.TabIndex = 0;
             // 
-            // iconPictureBox1
+            // label2
             // 
-            this.iconPictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.iconPictureBox1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Search;
-            this.iconPictureBox1.IconColor = System.Drawing.SystemColors.ControlText;
-            this.iconPictureBox1.IconSize = 26;
-            this.iconPictureBox1.Location = new System.Drawing.Point(3, 20);
-            this.iconPictureBox1.Name = "iconPictureBox1";
-            this.iconPictureBox1.Size = new System.Drawing.Size(26, 29);
-            this.iconPictureBox1.TabIndex = 9;
-            this.iconPictureBox1.TabStop = false;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(280, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Digite a área:";
             // 
-            // textBox1
+            // label1
             // 
-            this.textBox1.Location = new System.Drawing.Point(26, 20);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(197, 20);
-            this.textBox1.TabIndex = 8;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Procurar:";
             // 
-            // btneditar
+            // txtconsultararea
             // 
-            this.btneditar.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.btneditar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btneditar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btneditar.Location = new System.Drawing.Point(388, 384);
-            this.btneditar.Name = "btneditar";
-            this.btneditar.Size = new System.Drawing.Size(116, 37);
-            this.btneditar.TabIndex = 6;
-            this.btneditar.Text = "Editar";
-            this.btneditar.UseVisualStyleBackColor = false;
+            this.txtconsultararea.Location = new System.Drawing.Point(16, 36);
+            this.txtconsultararea.Name = "txtconsultararea";
+            this.txtconsultararea.Size = new System.Drawing.Size(130, 20);
+            this.txtconsultararea.TabIndex = 17;
+            this.txtconsultararea.TextChanged += new System.EventHandler(this.txtconsultararea_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Maroon;
+            this.panel2.Location = new System.Drawing.Point(249, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 421);
+            this.panel2.TabIndex = 16;
+            // 
+            // txtarea
+            // 
+            this.txtarea.Location = new System.Drawing.Point(283, 121);
+            this.txtarea.Name = "txtarea";
+            this.txtarea.Size = new System.Drawing.Size(223, 20);
+            this.txtarea.TabIndex = 15;
+            // 
+            // btnremover
+            // 
+            this.btnremover.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnremover.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnremover.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnremover.Location = new System.Drawing.Point(331, 329);
+            this.btnremover.Name = "btnremover";
+            this.btnremover.Size = new System.Drawing.Size(118, 37);
+            this.btnremover.TabIndex = 14;
+            this.btnremover.Text = "Remover";
+            this.btnremover.UseVisualStyleBackColor = false;
             // 
             // btnnovo
             // 
             this.btnnovo.BackColor = System.Drawing.SystemColors.ButtonShadow;
             this.btnnovo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnnovo.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnnovo.Location = new System.Drawing.Point(249, 384);
+            this.btnnovo.Location = new System.Drawing.Point(331, 196);
             this.btnnovo.Name = "btnnovo";
-            this.btnnovo.Size = new System.Drawing.Size(116, 37);
-            this.btnnovo.TabIndex = 5;
+            this.btnnovo.Size = new System.Drawing.Size(118, 37);
+            this.btnnovo.TabIndex = 13;
             this.btnnovo.Text = "Novo";
             this.btnnovo.UseVisualStyleBackColor = false;
             this.btnnovo.Click += new System.EventHandler(this.btnnovo_Click);
@@ -106,22 +134,25 @@
             this.dtareas.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dtareas.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
             this.dtareas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dtareas.Location = new System.Drawing.Point(3, 55);
+            this.dtareas.Location = new System.Drawing.Point(3, 62);
             this.dtareas.Name = "dtareas";
-            this.dtareas.Size = new System.Drawing.Size(240, 366);
+            this.dtareas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtareas.Size = new System.Drawing.Size(240, 359);
             this.dtareas.TabIndex = 10;
+            this.dtareas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dtareas_MouseClick);
             // 
-            // button1
+            // btneditar
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(605, 387);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(77, 37);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Editar";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btneditar.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btneditar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btneditar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btneditar.Location = new System.Drawing.Point(331, 259);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(118, 37);
+            this.btneditar.TabIndex = 6;
+            this.btneditar.Text = "Editar";
+            this.btneditar.UseVisualStyleBackColor = false;
+            this.btneditar.Click += new System.EventHandler(this.btneditar_Click);
             // 
             // consultar_areas
             // 
@@ -133,7 +164,6 @@
             this.Text = "consultar_areas";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dtareas)).EndInit();
             this.ResumeLayout(false);
 
@@ -143,10 +173,13 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btneditar;
-        private System.Windows.Forms.Button btnnovo;
-        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dtareas;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtconsultararea;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtarea;
+        private System.Windows.Forms.Button btnremover;
+        private System.Windows.Forms.Button btnnovo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- src/Forms/Forms_principais/Form_configuracoes/Área/consultar_areas.cs
+++ src/Forms/Forms_principais/Form_configuracoes/Área/consultar_areas.cs
@@ -9,23 +9,189 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace PSI18H_M16_Projeto_2218088_RodrigoBarata.Forms.Forms_principais.Form_configuracoes.Área
 {
     public partial class consultar_areas : Form
     {
-       
+        
+        DataTable dt = new DataTable();
+        string strSQL;
         public consultar_areas()
         {
             InitializeComponent();
-            
-            
+            DataGridView();
+
+
+
         }
 
         private void btnnovo_Click(object sender, EventArgs e)
         {
-            
-        }
-        
+            DB db = new DB();
+            try
+            {
+                
+                MySqlCommand command = new MySqlCommand("INSERT INTO `area`(`nome_area`) VALUES (@nome)", db.getConnection());
 
+                command.Parameters.Add("@nome", MySqlDbType.VarChar).Value = txtarea.Text;
+                
+
+                db.openConnection();
+
+                if (!checkTextBoxesValues())
+                {
+                    if (checkUsername())
+                    {
+                        MessageBox.Show("Já existe esta Área, escolha outra");
+                    }
+                    else
+                    {
+                        if (command.ExecuteNonQuery() == 1)
+                        {
+                            MessageBox.Show("Conta Criada");
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("Erro");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Preencha todos os campos");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                db.closeConnection();
+
+            }
+        }
+        public Boolean checkTextBoxesValues()
+        {
+            if (txtarea.Equals("") )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Boolean checkUsername()
+        {
+            DB db = new DB();
+
+            String nome = txtarea.Text;
+
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM area WHERE nome_area = @nome", db.getConnection());
+
+            command.Parameters.Add("@nome", MySqlDbType.VarChar).Value = nome;
+
+            adapter.SelectCommand = command;
+
+            adapter.Fill(table);
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void DataGridView()
+        {
+            DB db = new DB();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand cmd;
+
+            strSQL = "SELECT * FROM area ";
+            cmd = new MySqlCommand(strSQL, db.getConnection());
+            adapter.SelectCommand = cmd;
+
+            adapter.Fill(dt);
+
+            dtareas.DataSource = dt;
+
+            
+
+        }
+      
+
+        private void txtconsultararea_TextChanged(object sender, EventArgs e)
+        {
+            DataView dv = new DataView(dt);
+            dv.RowFilter = string.Format("nome_area LIKE '%{0}%'", txtconsultararea.Text);
+
+            dtareas.DataSource = dv;
+        }
+
+        private void dtareas_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtarea.Text = dtareas.CurrentRow.Cells[0].Value.ToString();
+        }
+
+        private void btneditar_Click(object sender, EventArgs e)
+        {
+             DB db = new DB();
+            try
+            {
+                
+                MySqlCommand command = new MySqlCommand("INSERT INTO `area`(`nome_area`) VALUES (@nome)", db.getConnection());
+
+                command.Parameters.Add("@nome", MySqlDbType.VarChar).Value = txtarea.Text;
+                
+
+                db.openConnection();
+
+                if (!checkTextBoxesValues())
+                {
+                    if (checkUsername())
+                    {
+                        MessageBox.Show("Já existe esta Área, escolha outra");
+                    }
+                    else
+                    {
+                        if (command.ExecuteNonQuery() == 1)
+                        {
+                            MessageBox.Show("Conta Criada");
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("Erro");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Preencha todos os campos");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                db.closeConnection();
+
+            }
+        }
     }
 }
